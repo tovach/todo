74d55982baae5a5a53b87ffcfc24f2b2787820bb
tovach <red5.dalv@gmail.com>
2022-02-12 20:56:26 +0300

    refactor: add query params

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,17 +1,22 @@
 import {useEffect, useState} from "react";
 import {TodoItem} from "../types";
 
-export const useTodos = () => {
-    const url = 'https://jsonplaceholder.typicode.com/todos';
+export const useGetTodos = (page: number, limit: number=10) => {
+    const url = `https://jsonplaceholder.typicode.com/todos/?_page=${page}&_limit=${limit}`;
     const [data, setData] = useState<TodoItem[]>();
     const [error, setError] = useState(false);
+    const [totalPages, setTotalPages] = useState(0);
     const [loading, setLoading] = useState(true);
 
 
     const getTodos = async () => {
         try {
             const response = await fetch(url);
+            const headers = response.headers.get('x-total-count');
             const result: TodoItem[] = await response.json();
+
+            setTotalPages(Math.ceil(parseInt(headers || '') / limit))
+
             setData(result);
             setError(false);
         } catch (e) {
@@ -25,6 +30,6 @@ export const useTodos = () => {
     useEffect(() => {
             getTodos()
         }
-        , [])
-    return {data, error, loading}
+        , [page])
+    return {data, error, loading, totalPages}
 }
